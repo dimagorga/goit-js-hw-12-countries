84ed1ee1d8cc2e2baa98bfd598a2699faa764957
Dzmitry Harhun <dimagorga@gmail.com>
2021-07-01 12:54:59 +0300

    добавляет поиск стран

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './sass/main.scss';
-import templateMarkup from './templates/country-template.hbs'
-
+import templateCountryMarkup from './templates/country-template.hbs'
+import templateManyCountriesMurkup from './templates/country-list.hbs'
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import { alert } from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+defaultModules.set(PNotifyMobile, {});
 
 const inputRef = document.querySelector('.input-js')
 const debounce = require('lodash.debounce');
@@ -10,16 +16,48 @@ const countryContainer = document.querySelector('.country-container')
 inputRef.addEventListener('input', debounce(countrySerch, 1000))
 
 
-function countrySerch(){
-    console.log()
-    fetch(`https://restcountries.eu/rest/v2/name/${inputRef.value}`)
-    .then(response => {return response.json()})
-    .then(countries =>  {
-        const markup =  templateMarkup(countries)
-        console.log(countries);
-        countryContainer.innerHTML= markup
+function countrySerch(){   
+    clearInput()
+    fetchCountries()
+    .then(makeCountrySearcher)
+    .catch(error => console.log(error))   
+}
+
+function makeCountrySearcher(countries){
+    if(countries.length > 1 && countries.length <= 10){
+        createCountryListMarkup(countries)
+    }else if(countries.length > 10){
+        createAlert(countries)
+    }else{ 
+        createCountryMarkup(countries)
+    } 
+}
+
+function fetchCountries(){
+    return fetch(`https://restcountries.eu/rest/v2/name/${inputRef.value}`)
+    .then(response => {
+        return response.json()
     })
-   
+}
+
+function clearInput(){
+    countryContainer.innerHTML = '';
+}
+
+function createCountryMarkup(countries){
+    const countryMarkup =  templateCountryMarkup(...countries)   
+    countryContainer.insertAdjacentHTML('beforeend', countryMarkup)
+}
+
+function createCountryListMarkup(countries){
+    const manyCountriesMarkup = templateManyCountriesMurkup(countries)
+    countryContainer.insertAdjacentHTML('beforeend', manyCountriesMarkup)
+}
+
+function createAlert(){
+    return  alert({
+        text: 'Too many matches found. Please enter a more specific query!'
+      });
 }
 
 
